9f0d797d5080244fbd6d378538b2944f97913276
Raghav8817 <raghavsingh08724@gmail.com>
2026-03-28 22:47:49 +0530

    corrected for backend in render (app.js cors issue)

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,33 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './config/.env') });
 
 
+const allowedOrigins = [
+    process.env.FRONTEND_URL,      // Your Netlify URL
+    "http://localhost:5173",       // Your Local React URL
+    "http://127.0.0.1:5173"        // Just in case
+];
+
 app.use(cors({
-    // Fallback to localhost if the environment variable isn't set
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
+    origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+// app.use(cors({
+//     // Fallback to localhost if the environment variable isn't set
+//     origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     // allowedHeaders: ["Content-Type", "Authorization"]
+// }));
 app.use(express.json())
 app.use(cookieparser())
 
