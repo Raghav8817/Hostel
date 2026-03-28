5a2ffcb29043a58ed48076061f14f793aae76bb6
Raghav8817 <raghavsingh08724@gmail.com>
2026-03-28 22:38:34 +0530

    corrected for backend in render

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,12 @@ require('dotenv').config({ path: path.resolve(__dirname, './config/.env') });
 
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Your React URL
-    credentials: true 
-}))
+    // Fallback to localhost if the environment variable isn't set
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json())
 app.use(cookieparser())
 
